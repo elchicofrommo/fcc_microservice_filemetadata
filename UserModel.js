c68a92fd12ab53d64c515da2364929e2cb17f84f
Mario Hernandez <mario.hernandez.eng@gmail.com>
2020-03-08 10:07:46 -0700

    finally have a working babel 7 env with jest

--- UserModel.js
+++ UserModel.js
@@ -1,4 +1,28 @@
 var sfy = JSON.stringify;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-const validateURL = require("./url-validation.js").validateURL;
+var Schema = mongoose.Schema;
+
+import logger from './utils/Logger';
+
+var UserSchema =  new Schema ({
+	user_name: String,
+	user_id: Number
+});
+
+var UserModel = mongoose.model('User', UserSchema, 'fitness_user');
+
+UserModel.findUser  = async function(userName){
+
+	let toReturn = undefined;
+	try{
+		toReturn = await User.findOne({user_name: userName}).select('user_name user_id -_id');
+		logger.info("found: " + toReturn);
+	}catch(err){
+		logger.error('failed to find short url for ' + url + " due to " + err);
+	}
+	logger.info("findShortUrl returning " + toReturn);
+	return  toReturn
+}
+
+export default UserModel
---
+++ UserModel.test.js
@@ -0,0 +1,27 @@
+import logger from './utils/Logger'
+import UserModel from './UserModel'
+
+const MongodbMemoryServer = require('mongodb-memory-server')
+const mongoServer = new MongodbMemoryServer.MongoMemoryServer()
+
+
+const mongoose = require('mongoose');
+
+beforeAll(async()=> {
+	global.__MONGO_URI__ = await mongoServer.getConnectionString()
+	logger.info(`global uri is ${global.__MONGO_URI__}`)
+    await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+        if (err) {
+            logger.error(err);
+            process.exit(1);
+        }
+    });
+
+   	return;
+
+})
+
+
+test("Testing can get the UserModel object", ()=>{
+	expect(UserModel).toBeTruthy();
+});
